9b1fc7d45310d2f390216a5e80a5bb7d50520936
recomplete <wantop@jbnu.ac.kr>
2022-09-24 14:08:02 +0900

    결과 이미지 띄우기 성공

--- src/main/java/com/hoiae/mygoods/product/controller/ProductController.java
+++ src/main/java/com/hoiae/mygoods/product/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.hoiae.mygoods.product.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.simple.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -31,19 +33,7 @@ public class ProductController {
     public String uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
 
         System.out.println(file.getOriginalFilename());
-//
-//        /* RestTemplate instance*/
-//        RestTemplate restTemplate = new RestTemplate();
-//
-//        /*request header*/
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-//
-//
-//        /*request body*/
-//        MultiValueMap<String, Object> body  = new LinkedMultiValueMap<>();
-//        body.add("file", file);
-//
+
 //        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 //
 //        String serverUrl = "http://192.168.0.58:5000/facev1";
@@ -55,48 +45,27 @@ public class ProductController {
 
         //RestTemplate을 이용한 단일 파일 업로드
 
+        /*  create RestTemplate instance*/
         RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders(); //헤더와 본문이 있는 HttpEntity를 만든다.
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA); //헤더값을 설정해주면 RestTemplate은 일부 메타 데이터와 함께 파일 데이터를 자동으로 마샬링 한다.
 
+        /*request header*/
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+
+        /*request body*/
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<String, Object>(); //LinkedMultiValueMap은 LinkedList의 각 키에 대해 여러 값을 저장하는 LinkedHashMap을 래핑
+        System.out.println(file.getResource());
         body.add("file", file.getResource()); //리소스 보내기
 
-        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<MultiValueMap<String,Object>>(body, headers); //헤더와 본문 개체를 감싸는 HttpEntity 인스턴스를 생성하고 RestTemplate을 사용하여 게시한다.
-        ResponseEntity<String> response = restTemplate.postForEntity("http://192.168.0.58:5000/facev1", entity, String.class); //restTemplate.postForEntity() 호출은 주어진 URL에 연결하고 파일을 서버로 보내는 작업을 완료
+        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<MultiValueMap<String, Object>>(body, headers); //헤더와 본문 개체를 감싸는 HttpEntity 인스턴스를 생성하고 RestTemplate을 사용하여 게시한다.
+        ResponseEntity<String> response = restTemplate.postForEntity("https://photo-to-character-rvakk.run.goorm.io/Shinkai", entity, String.class); //restTemplate.postForEntity() 호출은 주어진 URL에 연결하고 파일을 서버로 보내는 작업을 완료
 
         System.out.println(response.getBody());
 
+//        Object obj = new JSONParser().parse(response.getBody());
 
+        return response.getBody();
 
-        return "redirect:/";
     }
-
-
-
-
-
-/*    @PostMapping("/fileupload")
-    public String fileUpload(@RequestParam MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
-        File converFile = new File(file.getOriginalFilename());
-
-        System.out.println(file);
-
-    @PostMapping("/api")
-    @ResponseBody
-    public String uploadImage(MultipartHttpServletRequest request){
-        System.out.println(request.getSession());
-        return "OK";
     }
-
-    @PostMapping("/fileupload")
-    public String fileUpload(@RequestParam MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
-        File converFile = new File(file.getOriginalFilename());
-
-        System.out.println(converFile.getName());
-
-
-    }*/
-
-
-}
