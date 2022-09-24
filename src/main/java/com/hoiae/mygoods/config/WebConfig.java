f1fb936dc4bd1cb41d0805b2c5f4b0e7a3c36d2e
recomplete <wantop@jbnu.ac.kr>
2022-09-25 02:47:42 +0900

    ㅇjcrop test

---
+++ src/main/java/com/hoiae/mygoods/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.hoiae.mygoods.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/", "classpath:/static/");
+
+        // 이미지 불러올수 있도록 세팅
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("classpath:/upload/");
+    }
+}
