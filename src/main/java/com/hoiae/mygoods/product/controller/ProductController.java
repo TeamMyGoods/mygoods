84758837d4a8ae8fd425125be43d29dfbf039481
recomplete <wantop@jbnu.ac.kr>
2022-09-28 17:58:00 +0900

    ㄹã캐릭터 파일저장

--- src/main/java/com/hoiae/mygoods/product/controller/ProductController.java
+++ src/main/java/com/hoiae/mygoods/product/controller/ProductController.java
@@ -19,6 +19,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 @Controller
@@ -42,10 +44,85 @@ public class ProductController {
 
     @ResponseBody
     @PostMapping("/character")
-    public String uploadCharacter(@RequestParam("image") MultipartFile image) throws ParseException {
+    public String uploadCharacter(@RequestParam("image") MultipartFile image) throws ParseException, IOException {
 
 
         System.out.println("image : " + image.getOriginalFilename());
+
+
+        String rootLocation = IMAGE_DIR;
+
+        String fileUploadDirectory = rootLocation + "/upload/original";
+        File directory = new File(fileUploadDirectory);
+
+        if(!directory.exists()){
+            directory.mkdirs();
+        }
+
+        List<Map<String, String>> fileList = new ArrayList<>();
+
+        List<MultipartFile> paramFileList = new ArrayList<>();
+        paramFileList.add(image);
+
+        try {
+            for (MultipartFile paramFile : paramFileList) {
+                if (paramFile.getSize() > 0) {
+                    String originFileName = paramFile.getOriginalFilename();
+
+                    String ext = originFileName.substring(originFileName.lastIndexOf("."));
+                    String savedFileName = "(c)" + UUID.randomUUID().toString().replace("-", "") + ext;
+
+
+                    paramFile.transferTo(new File(fileUploadDirectory + "/" + savedFileName));
+
+                    /* DB에 업로드한 파일의 정보를 저장하는 비지니스 로직 수행 */
+                    /* 필요한 정보를 Map에 담는다. */
+                    Map<String, String> fileMap = new HashMap<>();
+                    fileMap.put("originFileName", originFileName);
+                    fileMap.put("savedFileName", savedFileName);
+                    fileMap.put("savePath", fileUploadDirectory);
+
+                    System.out.println("파일경로 " +   fileUploadDirectory + "/" + savedFileName);
+
+
+                    CharacterDTO character  = new CharacterDTO();
+                    character.setMemberNo(1);
+                    character.setModelName("초상화 Hosoda ");
+                    character.setCharacterImageUrl(savedFileName);
+
+
+                    productService.registCharacter(character);
+
+
+                }
+            }
+
+        } catch (IllegalStateException | IOException e) {
+            e.printStackTrace();
+
+            /* 어떤 종류의 Exception이 발생 하더라도실패 시 파일을 삭제해야 한다. */
+            int cnt = 0;
+            for (int i = 0; i < fileList.size(); i++) {
+                Map<String, String> file = fileList.get(i);
+
+                File deleteFile = new File(fileUploadDirectory + "/" + file.get("savedFileName"));
+                boolean isDeleted1 = deleteFile.delete();
+
+
+
+                if (isDeleted1) {
+                    cnt++;
+                }
+            }
+
+            if (cnt == fileList.size()) {
+                e.printStackTrace();
+            } else {
+                e.printStackTrace();
+            }
+        }
+
+
 
         //RestTemplate을 이용한 단일 파일 업로드
 
@@ -77,18 +154,19 @@ public class ProductController {
         JSONObject jsonObj = (JSONObject) obj;
         System.out.println("json['result'] : " + jsonObj.get("result"));
         System.out.println("json['img'] : " + jsonObj.get("img"));
-        System.out.println("getbody" + response.getBody());
 
-        CharacterDTO character  = new CharacterDTO();
-        character.setMemberNo(1);
-        character.setModelName("테스트모델");
-        character.setCharacterImageUrl("test.png");
+        String changeImg = (String) jsonObj.get("img");
+        byte[] binary = Base64.getDecoder().decode(changeImg);
+        System.out.println("binary" + binary);
 
+        String savedFileName = "(c)" + UUID.randomUUID().toString().replace("-", "");
+        Files.write(Paths.get("D:\\mygoods\\mygoods\\src\\main\\resources\\upload\\change\\"+savedFileName+".jpg"), binary);
 
-        productService.registCharacter(character);
 
         return response.getBody();
     }
+
+
 
     @ResponseBody
     @PostMapping("/upload")
@@ -106,7 +184,7 @@ public class ProductController {
 
         String rootLocation = IMAGE_DIR;
 
-        String fileUploadDirectory = rootLocation +"/upload/original";
+        String fileUploadDirectory = rootLocation + "/upload/original";
         File directory = new File(fileUploadDirectory);
 
         if(!directory.exists()){
@@ -124,7 +202,7 @@ public class ProductController {
                     String originFileName = paramFile.getOriginalFilename();
 
                     String ext = originFileName.substring(originFileName.lastIndexOf("."));
-                    String savedFileName = UUID.randomUUID().toString().replace("-", "") + ext;
+                    String savedFileName = "(p)" + UUID.randomUUID().toString().replace("-", "") + ext;
 
 
                     paramFile.transferTo(new File(fileUploadDirectory + "/" + savedFileName));
@@ -139,7 +217,6 @@ public class ProductController {
                     System.out.println("파일경로 " +   fileUploadDirectory + "/" + savedFileName);
 
                     ProductDTO product = new ProductDTO();
-                    product.setProductCode(1);
                     product.setProductName("나만의 에코백");
                     product.setProductPrice(10000);
                     product.setCategoryCode(3);
@@ -147,59 +224,9 @@ public class ProductController {
                     product.setProductImageUrl(savedFileName);
 
                     productService.registProduct(product);
-//
-//                    /* 제목 사진과 나머지 사진을 구분하고 썸네일도 생성한다. */
-//                    int width = 0;
-//                    int height = 0;
-//
-//                    String fieldName = paramFile.getName();
-//
-//                    if ("thumbnailImg1".equals(fieldName)) {
-//                        fileMap.put("fileType", "TITLE");
-//
-//                        /* 썸네일로 변환 할 사이즈를 지정한다. */
-//                        width = 300;
-//                        height = 150;
-//                    } else {
-//                        fileMap.put("fileType", "BODY");
-//
-//                        width = 120;
-//                        height = 100;
-//                    }
-//
-//                    /* 썸네일로 변환 후 저장한다. */
-//                    Thumbnails.of(fileUploadDirectory + "/" + savedFileName).size(width, height)
-//                            .toFile(thumbnailDirectory + "/thumbnail_" + savedFileName);
-//
-//                    /* 나중에 웹서버에서 접근 가능한 경로 형태로 썸네일의 저장 경로도 함께 저장한다. */
-//                    fileMap.put("thumbnailPath", "/upload/thumbnail/thumbnail_" + savedFileName);
-//
-//                    fileList.add(fileMap);
+
                 }
             }
-
-
-//            /* 서비스를 요청할 수 있도록 BoardDTO에 담는다. */
-//
-//            thumbnail.setAttachmentList(new ArrayList<AttachmentDTO>());
-//            List<AttachmentDTO> list = thumbnail.getAttachmentList();
-//            for (int i = 0; i < fileList.size(); i++) {
-//                Map<String, String> file = fileList.get(i);
-//
-//                AttachmentDTO tempFileInfo = new AttachmentDTO();
-//                tempFileInfo.setOriginalName(file.get("originFileName"));
-//                tempFileInfo.setSavedName(file.get("savedFileName"));
-//                tempFileInfo.setSavePath(file.get("savePath"));
-//                tempFileInfo.setFileType(file.get("fileType"));
-//                tempFileInfo.setThumbnailPath(file.get("thumbnailPath"));
-//
-//                list.add(tempFileInfo);
-//            }
-//
-//
-//            boardService.registThumbnail(thumbnail);
-//
-//            rttr.addFlashAttribute("message", "사진 게시판 등록에 성공하셨습니다.");
 
         } catch (IllegalStateException | IOException e) {
             e.printStackTrace();
@@ -212,8 +239,7 @@ public class ProductController {
                 File deleteFile = new File(fileUploadDirectory + "/" + file.get("savedFileName"));
                 boolean isDeleted1 = deleteFile.delete();
 
-//                File deleteThumbnail = new File(thumbnailDirectory + "/thumbnail_" + file.get("savedFileName"));
-//                boolean isDeleted2 = deleteThumbnail.delete();
+
 
                 if (isDeleted1) {
                     cnt++;
